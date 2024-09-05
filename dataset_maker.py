7877313ba31e091f09d32f9e85a0096c3b107961
K3dA2 <ayanfeoluwaadekanye1@gmail.com>
2024-09-05 12:36:44 +0100

    Update dataset_maker.py

--- dataset_maker.py
+++ dataset_maker.py
@@ -3,56 +3,33 @@ import numpy as np
 from tqdm import tqdm
 import os
 from models.model import VQVAE 
-from utils.utils import get_data_loader, count_parameters, save_img_tensors_as_grid
+from utils import get_data_loader, count_parameters, save_img_tensors_as_grid
 import matplotlib.pyplot as plt
 
 def data_loop(n_epochs, optimizer, model, loss_fn, device, data_loader, max_grad_norm=1.0, epoch_start=0, save_img=True, show_img=False):
     indices_list = []
+    with torch.no_grad():
+        for epoch in range(epoch_start, n_epochs):
+            progress_bar = tqdm(data_loader, desc=f'Epoch {epoch}', unit='batch')
+            for imgs, _ in progress_bar:
+                imgs = imgs.to(device)
+                indices = model.return_indices(imgs)
+                out,_ = model(imgs)
+                indices_list.append(indices.cpu().numpy())
 
-    for epoch in range(epoch_start, n_epochs):
-        progress_bar = tqdm(data_loader, desc=f'Epoch {epoch}', unit='batch')
-        for imgs, _ in progress_bar:
-            imgs = imgs.to(device)
-            indices = model.return_indices(imgs)
-            out,_ = model(imgs)
-            indices_list.append(indices.cpu().numpy())
-            print(indices[0].unsqueeze(0).shape)
-            with torch.no_grad():
-                img,z = model.decode(indices[0].unsqueeze(0))
-            out = out[0].unsqueeze(0).detach()
-            #img = model.decoder(c)
-            img = img[0].unsqueeze(0).detach()
-
-            if torch.equal(out,img):
-                print("yes")
-            else:
-                print("difference: ", (out - img).mean())
-            img = out
-            img = img.squeeze().permute(1, 2, 0).cpu().numpy()
-            mean=[0.7002, 0.6099, 0.6036]
-            std=[0.2195, 0.2234, 0.2097]
-            mean = np.array(mean)
-            std = np.array(std)
-            img = img * std + mean
-            img = np.clip(img, 0, 1)
-            img = (img * 255).astype(np.uint8)
-
-            plt.imshow(img)
-            plt.axis('off')
-            plt.show()
-            
-    return np.concatenate(indices_list)
+        return np.concatenate(indices_list)
 
 if __name__ == "__main__":
-    path = '/Users/ayanfe/Documents/Datasets/Waifus/Train'
-    val_path = '/Users/ayanfe/Documents/Datasets/MNIST Upscaled/Val'
-    model_path = 'weights/waifu-vqvae.pth'
+    path = ''
+    val_path = ''
+    model_path = ''
     
     device = "cuda" if torch.cuda.is_available() else "mps" if hasattr(torch.backends, "mps") and torch.backends.mps.is_available() else "cpu"
     print(f"Using device: {device}")
 
-    model = VQVAE(latent_dim = 3, num_embeddings = 512, beta=0.25, use_ema=False, e_width=64,d_width=64)  # Assuming Unet is correctly imported and defined
+    model = VQVAE(latent_dim = 64, num_embeddings=512, beta=0.25, use_ema=False, e_width=64,d_width=64)  # Assuming Unet is correctly imported and defined
     model.to(device)
+    model.eval()
     optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
     loss_fn = torch.nn.MSELoss().to(device)
     print(f"Model parameters: {count_parameters(model)}")
